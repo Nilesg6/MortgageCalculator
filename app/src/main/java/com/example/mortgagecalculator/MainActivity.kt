09a04fe429f1a0dfde57909afb464f6ed0e57b28
Nilesg6 <nilesg6@gmail.com>
2023-02-28 18:19:05 -0800

    Initial commit

---
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mortgagecalculator
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(____________________________________)
+
+    }
+}
